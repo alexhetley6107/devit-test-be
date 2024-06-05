c35fa6acacf77d1f7a16e5a1857400cf1ee72901
alexhetley6107 <hetleyofficial@gmail.com>
2024-06-05 10:07:06 +0300

    feat: delay

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import cors from 'cors';
 import { rateLimit } from 'express-rate-limit';
 
 const PORT = 5000;
+const MS_COEFFICIENT = 1000;
+
 const app = express();
-const MS_KOEF = 1000;
 
 const limiter = rateLimit({
   windowMs: 1000,
-  limit: 5,
+  limit: 50,
   standardHeaders: 'draft-7',
   legacyHeaders: false,
   message: 'LIMIT__ERROR',
@@ -18,15 +19,12 @@ app.use(limiter);
 app.use(cors());
 
 app.get('/api', (req, res) => {
-  const delay = Math.random * MS_KOEF;
+  const delay = Math.random * MS_COEFFICIENT;
 
   setTimeout(() => {
     const itemRef = +req.query.itemRef;
-    if (itemRef !== 100) {
-      res.status(200).json(itemRef);
-    } else {
-      res.status(429).json(null);
-    }
+
+    res.status(200).json(itemRef);
   }, delay);
 });
 
